44d56dfc7bcc07d0c5b7eb06411ceef27e72e134
nazarf <nazarfedisin@gmail.com>
2022-05-16 13:38:02 +0300

    clean up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type {NextPage} from 'next'
-import Header from '../components/Header'
 import Main from "../components/Main";
 import Useful from "../components/Useful";
 import MgContainer from "../components/mgcontainer";
