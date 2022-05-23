63f898c17a23d1733dc9d061789a3f1859fd5c80
nazarf <nazarfedisin@gmail.com>
2022-05-23 14:35:51 +0300

    fix header links

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,51 +3,44 @@ import {L} from "../../l10n/L10n"
 import Styles from "./Header.module.scss"
 import LayoutStyles from "../Layout.module.scss"
 import PlayBtn from "../PlayBtn/PlayBtn";
+import {PagePath} from "../../helpers/PageHelper";
 
 const Header = () => {
-
-
-
     return (
         <div className={Styles.header}>
             <div className={LayoutStyles.container}>
                 <div className={Styles.nav}>
                     <div>
-                        <a href="components/Header/Header">
+                        <a href={PagePath.INDEX}>
                             <img height={"44px"} src={mobile_logo_img.src} alt="mobile logo"/>
                         </a>
                     </div>
                     <div className={Styles.nav_box}>
-                        <a href="components/Header/Header">
+                        <a href={PagePath.INDEX}>
                             <li>{L.header.links.about_game}</li>
                         </a>
-                        <a href="components/Header/Header">
+                        <a href={PagePath.INDEX}>
                             <li>{L.header.links.my_assets}</li>
                         </a>
-                        <a href="components/Header/Header">
+                        <a href={PagePath.INDEX}>
                             <li>{L.header.links.blog}</li>
                         </a>
-                        <a href="components/Header/Header">
+                        <a href={PagePath.INDEX}>
                             <li>{L.header.links.wiki}</li>
                         </a>
-                        <a href="components/Header/Header">
+                        <a href={PagePath.INDEX}>
                             <li>{L.header.links.team}</li>
                         </a>
-                        <a href="components/Header/Header">
+                        <a href={PagePath.INDEX}>
                             <li>{L.header.links.rewards}</li>
                         </a>
-                        <a href="components/Header/Header">
+                        <a href={PagePath.INDEX}>
                             <li>{L.header.links.faqs}</li>
                         </a>
                     </div>
-                    <a className={Styles.playBtnHeader} href="components/Header/Header">
+                    <a href={PagePath.INDEX}>
                         <PlayBtn text={L.header.play_game}/>
                     </a>
-                </div>
-            </div>
-            <div className={Styles.mobMenu}>
-                <div className={Styles.menu}  >
-
                 </div>
             </div>
         </div>
