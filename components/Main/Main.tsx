9bca26b29aa3e3968289299f2bab1ce974b476ad
nazarf <nazarfedisin@gmail.com>
2022-05-23 14:30:40 +0300

    add page helper

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -3,6 +3,7 @@ import Logo_with_icon from '../../images/logo_with_icon.png'
 import PlayBtn from "../PlayBtn/PlayBtn";
 import LayoutStyles from "../Layout.module.scss";
 import {L} from "../../l10n/L10n"
+import {PagePath} from "../../helpers/PageHelper";
 
 const Main = () => {
   return(
@@ -16,10 +17,10 @@ const Main = () => {
                 </h2>
             </div>
              <div className={Styles.btn_container_main_page}>
-                 <a className={Styles.playBtnBig} href="components/Main/Main">
+                 <a className={Styles.playBtnBig} href={PagePath.INDEX}>
                      <PlayBtn text={L.main.play_game}/>
                  </a>
-                 <a href="components/Main/Main">
+                 <a href={PagePath.INDEX}>
                      <button className={Styles.secondPlayBtn}>{L.main.tutorial}</button>
                  </a>
              </div>
---
+++ helpers/PageHelper.tsx
@@ -0,0 +1,4 @@
+export enum PagePath {
+    INDEX = "/",
+    TEAM = "team"
+}
